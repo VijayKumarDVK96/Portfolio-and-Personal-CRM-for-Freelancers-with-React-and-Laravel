2e5673ae150e3c8e450b6f1f1c5ce9e693abe21f
VijayKumarCFX <146800024+VijayKumarCFX@users.noreply.github.com>
2026-01-26 13:18:19 +0530

    updated config to choose env.production

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -41,6 +41,8 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$app->loadEnvironmentFrom('.env.production');
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
